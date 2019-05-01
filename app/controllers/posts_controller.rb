1efa2bfcee397e2e1b760df9fc8c077ecd375e76
TamerL <judywu29@gmail.com>
2019-05-01 22:12:22 +1000

    practice of adding create and delete actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,34 @@ class PostsController < ActionController::Base
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(name: params[:post][:name], title: params[:post][:title], user_id: User.last.id)
+    redirect_to @post
+  end
+
+  def destroy
+    Post.find(params[:id]).delete
+    redirect_to action: :index
+  #   redirect_to :action => :index
+  #
+  #   array:
+  #   a =[1, 2, 3]
+  #   a.first: a[0]
+  #   a[1]
+  #   a[2]
+  #
+  #   hash = {a: :b, c: :d}
+  #   hash[:a] = :b
+  #   hash[:c]= :d
+  #
+  #   active record: rails model
+  #   user = User.create(name: 'judy', email: 'judy@fresho.com')
+  #   user.name
+  #   user.email
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ActionController::Base
     @posts = @user.posts
     @cars = @user.cars
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(name: params[:user][:name], email: params[:user][:email])
+    redirect_to @user
+  end
+
+  def destroy
+    User.find(params[:id]).delete
+    redirect_to :action => :index
+  end
 end
